842749bb2557c5007880b6a08d462696307c6a97
unknown <>
2017-04-06 20:34:53 +0900

    #9 버튼 여러개를 onClick에서 모두 처리하게 하기

--- app/src/main/java/hyerim/kr/hs/e_mirim/friendsimulation/MainActivity.java
+++ app/src/main/java/hyerim/kr/hs/e_mirim/friendsimulation/MainActivity.java
@@ -12,7 +12,28 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     void onClick(View view){
-        Toast.makeText(this, "작동해용><", Toast.LENGTH_SHORT).show();
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(this, "안녕", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "으아아아아", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this, "배고파", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this, "젤리 먹을래?", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this, "불끌게", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "뭐라는지 모르겠어염", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
