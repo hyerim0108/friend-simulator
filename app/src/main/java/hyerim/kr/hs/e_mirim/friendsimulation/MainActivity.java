a3f07d1d95006a81e8c5cd222e73a7d24293380e
unknown <>
2017-04-06 20:17:54 +0900

    #8 버튼 하나 작동완료

--- app/src/main/java/hyerim/kr/hs/e_mirim/friendsimulation/MainActivity.java
+++ app/src/main/java/hyerim/kr/hs/e_mirim/friendsimulation/MainActivity.java
@@ -2,6 +2,8 @@ package hyerim.kr.hs.e_mirim.friendsimulation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,6 +11,8 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+    }
+    void onClick(View view){
+        Toast.makeText(this, "작동해용><", Toast.LENGTH_SHORT).show();
     }
 }
